10008bdac645ff1d55604579c869a0f578e1eaf4
Omkar Patil <omkar.spatil@outlook.com>
2023-12-03 14:27:33 +0530

    Small Changes

---
+++ Lab5/Node.java
@@ -0,0 +1,34 @@
+class Node<E> implements Position<E> {
+    E element;
+    private Node<E> prev;
+    private Node<E> next;
+
+    public Node(E element, Node<E> prev, Node<E> next) {
+        this.element = element;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public void setElement(E e) {
+    }
+}
